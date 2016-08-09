c481a36d4f49c1b88ff51c06550f07ad75b6c11a
Muhammad Khattab <muhammad.khattab@hotmail.com>
2016-08-09 12:05:28 +0200

    Add files via upload

---
+++ mqm.py
@@ -0,0 +1,50 @@
+dict_file = open("C:/Users/muhammad.khattab/Workspace/Python/Hash/dict", 'r')
+dict = {}
+
+for dict_entry in dict_file:
+    ref = dict
+    tmp = dict_entry.rstrip('\n').split(' ')
+    for i in tmp:
+        if i not in ref:
+            ref[i]={}
+        ref = ref[i]
+        if (i == tmp[-1]):
+            ref['']=1
+# print(dict)
+
+def found_match(fm_print_line, fm_match, fm_found, fm_ref):
+	fm_print_line = fm_print_line+"<FOUND>"+fm_match.rstrip(' ')+"<\/FOUND> "
+	fm_found =1
+	fm_ref = dict
+	fm_match =""
+	return fm_print_line, fm_match, fm_found, fm_ref
+
+text_file = open("C:/Users/muhammad.khattab/Workspace/Python/Hash/text", 'r')
+for line in text_file:
+	found =0
+	ref = dict
+	switch =0
+	arr = line.rstrip('\n').split(' ')
+	match =""
+	print_line = ""
+	for i in range(len(arr)):
+		tmp = arr[i]
+		if (tmp in ref):
+			ref = ref[tmp]
+			match=match+tmp+' '
+			if ('' in ref):
+				print_line, match, found, ref = found_match(print_line, match, found, ref)
+		else:
+			ref = dict
+			if (tmp not in ref):
+				print_line=print_line+match+tmp+' '
+			else:
+				print_line=print_line + match
+				match =''
+				ref = ref[tmp]
+				match=tmp+' '
+				if ('' in ref):
+					print_line, match, found, ref = found_match(print_line, match, found, ref)
+
+	if found ==1:
+		print(print_line.rstrip(' '))
